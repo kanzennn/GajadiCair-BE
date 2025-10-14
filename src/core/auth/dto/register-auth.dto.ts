586a8440cf79fb78592a01c50d1d5e8272f628b7
Faiz Elfahad <faizelfahad2@gmail.com>
2025-10-15 00:55:12 +0700

    fix: register validation min

--- src/core/auth/dto/register-auth.dto.ts
+++ src/core/auth/dto/register-auth.dto.ts
@@ -1,14 +1,14 @@
-import { IsEmail, IsString, Min } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class RegisterAuthDto {
   @IsString()
-  @Min(4)
+  @MinLength(4)
   name: string;
 
   @IsEmail()
   email: string;
 
   @IsString()
-  @Min(6)
+  @MinLength(6)
   password: string;
 }
