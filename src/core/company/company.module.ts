7bd5edafdc5f5db477097b116f3f99b3c3a4d749
Faiz Elfahad <faizelfahad2@gmail.com>
2025-11-10 18:42:31 +0700

    feat(employee): send credential to email

--- src/core/company/company.module.ts
+++ src/core/company/company.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
 import { PrismaService } from 'src/common/services/prisma/prisma.service';
-import { EmployeeService } from '../employee/employee.service';
 import { BankService } from '../bank/bank.service';
+import { EmployeeModule } from '../employee/employee.module';
 
 @Module({
+  imports: [EmployeeModule],
   controllers: [CompanyController],
-  providers: [CompanyService, EmployeeService, BankService, PrismaService],
+  providers: [CompanyService, BankService, PrismaService],
+  exports: [CompanyService],
 })
 export class CompanyModule {}
